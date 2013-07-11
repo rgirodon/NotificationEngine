bf317e6d103f6e9f62c495c1dce43677d118d7b1
rgirodon <rgirodon@sqli.com>
2013-07-11 22:02:02 +0200

    Put a Http Controller for creating rawnotifications with a json http put request

---
+++ src/main/java/org/notificationengine/dto/RawNotificationDTO.java
@@ -0,0 +1,44 @@
+package org.notificationengine.dto;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class RawNotificationDTO {
+
+	private String topic;
+
+	private Map<String, Object> context;
+
+	public RawNotificationDTO() {
+		
+		super();
+		
+		this.context = new HashMap<>();
+	}
+	
+	
+	
+	@Override
+	public String toString() {
+		return "RawNotificationDTO [topic=" + topic + ", context=" + context
+				+ "]";
+	}
+
+
+
+	public String getTopic() {
+		return topic;
+	}
+
+	public void setTopic(String topic) {
+		this.topic = topic;
+	}
+	
+	public Map<String, Object> getContext() {
+		return context;
+	}
+
+	public void setContext(Map<String, Object> context) {
+		this.context = context;
+	}
+}
---
+++ src/main/java/org/notificationengine/web/controller/RawNotificationController.java
@@ -0,0 +1,61 @@
+package org.notificationengine.web.controller;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.log4j.Logger;
+import org.bson.types.ObjectId;
+import org.notificationengine.domain.RawNotification;
+import org.notificationengine.domain.Topic;
+import org.notificationengine.dto.RawNotificationDTO;
+import org.notificationengine.persistance.Persister;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@Controller
+public class RawNotificationController {
+	
+	private static Logger LOGGER = Logger.getLogger(RawNotificationController.class);
+	
+	@Autowired
+	private Persister persister;
+		
+	public RawNotificationController() {
+		
+		LOGGER.debug("RawNotificationController instantiated and listening");
+	}
+
+	@RequestMapping(value = "/rawNotification.do", method = RequestMethod.PUT, consumes = "application/json")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void create(@RequestBody RawNotificationDTO rawNotificationDTO) {
+		
+		LOGGER.debug("RawNotificationController received : " + rawNotificationDTO);
+		
+		RawNotification rawNotification = new RawNotification();
+		rawNotification.set_id(new ObjectId());
+		rawNotification.setProcessed(Boolean.FALSE);
+		rawNotification.setTopic(new Topic(rawNotificationDTO.getTopic()));
+		rawNotification.setContext(rawNotificationDTO.getContext());
+		
+		persister.createRawNotification(rawNotification);
+		
+		LOGGER.debug("RawNotification persisted : " + rawNotificationDTO);
+    }
+	
+	public Persister getPersister() {
+		return persister;
+	}
+
+	public void setPersister(Persister persister) {
+		this.persister = persister;
+	}
+}
